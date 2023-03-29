1d39debf3babbe0f24bcbd29109ebfa20355f4c4
unknown <Automacao@DESKTOP-9TJOKT2.(none)>
2023-03-29 09:33:44 -0300

    Projeto Python

---
+++ Snap7.py
@@ -0,0 +1,11 @@
+# Snap7
+# Description: Project to Snap7 Librarie
+# Author: Rafael Kruger Schwertz
+# Creation date: 2023-03-29
+# Last update: 2023-03-29
+# Version: 1.0
+# Platform: Windows 10
+# Programming language: Python
+# Libraries used: N/A
+# Additional Notes: N/A
+
